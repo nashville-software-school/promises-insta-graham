61dd3e050e7fa81ba52cd296204ce59a56c89d33
Alejandro Gutierrez Gongora <alex.gtz17@gmail.com>
2015-09-26 11:40:57 -0500

    getBooks promise first try

--- javascripts/promises.js
+++ javascripts/promises.js
@@ -4,7 +4,8 @@ requirejs.config({
     'jquery': '../lib/bower_components/jquery/dist/jquery.min',
     'lodash': '../lib/bower_components/lodash/lodash.min',
     'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
-    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min'
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'q':'../lib/bower_components/q/q'
   },
   shim: {
     'bootstrap': ['jquery']
@@ -12,8 +13,8 @@ requirejs.config({
 });
 
 requirejs(
-  ["jquery", "hbs", "bootstrap", "get-books"], 
-  function($, Handlebars, bootstrap, books) {
+  ["jquery", "hbs", "bootstrap", "get-books", "get-booktypes", "q"],
+  function($, Handlebars, bootstrap, books, booktypes, Q) {
 
     books.load(function(bookArray) {
       require(['hbs!../templates/books'], function(bookTpl) {
@@ -32,7 +33,7 @@ requirejs(
         // add the type key to each book that is currently
         // being performed in the get-books file
 
-        // then bind the template to the data 
+        // then bind the template to the data
         // (p.s. make the handlebar template a module dependency)
         require(['hbs!../templates/books'], function(bookTpl) {
           $("#bookList").html(bookTpl({ books:bookArray }));
